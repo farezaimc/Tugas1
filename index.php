d74fdd3dd71c1015c09cdfacc1af75d5606a2dfb
farezaimc <30324361+farezaimc@users.noreply.github.com>
2021-09-01 02:13:32 +0700

    Add files via upload
    silahkan dikembangkan sendiri, untuk form daftar,login,dll nya.

---
+++ index.php
@@ -0,0 +1,117 @@
+<html>
+
+<head>
+<?php
+date_default_timezone_set('Asia/Jakarta');
+?>
+<meta charset="utf-8">
+<meta name="viewport" content="witdh=device-witdh, initial-scale=1">
+<title>IND7</title>
+
+</head>
+<header>
+	<a href="#">Home</a>
+	<a href="#">Pesanan Saya</a>
+	<a href="#" style="float: right;">Masuk</a>
+	<a href="#" style="float: right;">Daftar</a>
+</header>
+
+<div style="padding-top: 5px;"></div>
+
+<div id="sidebar">
+<ul>
+<li><a href="#">Elektronik</a></li>
+<li><a href="#">Fashion</a></li>
+<li><a href="#">Beauty</a></li>
+<li><a href="#">Mainan</a></li>
+<li><a href="#">Otomotif</a></li>
+<li><a href="#">Perlengkapan rumah</a></li>
+<li><a href="#">Perlengkapan rumah tangga</a></li>
+<li><a href="#">Sport & Lifestyle</a></li>
+</ul>
+</div>
+<body>
+	<div id="wrapper">
+<div style="padding-top: 10px;">
+
+<fieldset class="fieldset1">
+<center><b style="font-size: 20px;">DAFTAR UTANG 2021</b></center>
+
+<table border="1px" style="border-collapse: collapse;" class="center">
+	
+	
+<tr>
+<th colspan="2">NO</th>
+<th rowspan="2">NAMA</th>
+<th rowspan="2">JUMLAH</th>
+<th rowspan="2">TANGGAL</th>
+<th rowspan="2">JAM</th>
+</tr>
+
+	
+<tr style="text-align: center;">
+<td><b>URUT</b></td>
+<td><b>NIK</b></td>
+</tr>
+<tr style="text-align: center;">
+<td>1</td>
+<td>3174030292</td>
+<td>Mr.Hasan</td>
+<td>Rp2.000.000-,</td>
+<td>25/08/2021</td>
+<td><?php echo date("h:i:s"); ?></td>
+</tr>
+<tr style="text-align: center;">
+<td>2</td>
+<td>3174022019</td>
+<td>Mr.Astomo</td>
+<td>Rp1.750.000-,</td>
+<td>26/08/2021</td>
+<td><?php echo date("h:i:s"); ?></td>
+</tr>
+<tr style="text-align: center;">
+<td>3</td>
+<td>3174099201</td>
+<td>Mr.Toni</td>
+<td>Rp1.350.000-,</td>
+<td>27/08/2021</td>
+<td><?php echo date("h:i:s"); ?></td>
+</tr>
+
+</table>
+</div>
+</fieldset>
+
+<div style="padding-top: 10px;">
+<fieldset class="fieldset2">
+<center><p>CONTENT 2</p></center>
+</fieldset>
+</div>
+
+<div style="padding-top: 10px;">
+<fieldset class="fieldset3">
+<center><p>CONTENT 3</p></center>
+</fieldset>
+</div>
+
+</div>
+
+<div style="padding-top: 5px;"></div>
+
+<div class="siders">
+<ul>
+<li><span>CONTENT 4</span></li>
+<li><span>CONTENT 5</span></li>
+<li><span>CONTENT 6</span></li>
+<li><span>CONTENT 7</span></li>
+<li><span>CONTENT 8</span></li>
+<li><span>CONTENT 9</span></li>
+<li><span>CONTENT 10</span></li>
+</ul>
+</div>
+
+<footer><p style="text-align: center;">Website create by <a href="#">IND7</a></p></footer>
+</body>
+<link rel="stylesheet" type="text/css" href="app.css?version=<?php echo filemtime('./app.css'); ?>">
+
+</html>
